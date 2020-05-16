90c932159beaa5d78036df079ae801ff56766f09
Zach "theY4Kman" Kanzler <they4kman@gmail.com>
2020-05-16 18:06:33 -0400

    chore: add marisa-trie req, and pytest-benchmark test req

--- setup.py
+++ setup.py
@@ -80,14 +80,14 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    #install_requires=[],
+    install_requires=['marisa-trie'],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
     # for example:
     # $ pip install -e .[dev,test]
     extras_require={
-        'test': ['pytest'],
+        'test': ['pytest', 'pytest-benchmark'],
     },
 
     # If there are data files included in your packages that need to be
