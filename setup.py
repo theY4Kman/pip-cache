03523f7979534ebaf1680458bb95c98c2f9805e0
Zach "theY4Kman" Kanzler <they4kman@gmail.com>
2020-05-16 18:06:15 -0400

    chore: don't install package cache w/ package

--- setup.py
+++ setup.py
@@ -9,7 +9,6 @@ from setuptools import setup, find_packages
 # To use a consistent encoding
 from codecs import open
 from os import path
-from pip_cache.xdg import get_xdg_data_dir
 
 here = path.abspath(path.dirname(__file__))
 
@@ -102,9 +101,7 @@ setup(
     # need to place data files outside of your packages. See:
     # http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files # noqa
     # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
-    data_files=[
-        (path.join(get_xdg_data_dir(), 'pip-cache'), ['data/all-packages.txt'])
-    ],
+    data_files=[],
 
     # To provide executable scripts, use entry points in preference to the
     # "scripts" keyword. Entry points provide cross-platform support and allow
